8d4c863950cefb9026abe75071c08fee742ee779
TaianSong <64883610+TaianSong@users.noreply.github.com>
2020-11-19 16:34:16 -0800

    Create Constant.h

---
+++ Constant.h
@@ -0,0 +1,21 @@
+
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#define WIDTH 600	/* image width */
+#define HEIGHT 400	/* image height */
+#define SLEN 80     /* maximum length of file names and string*/
+#define SUCCESS 0   /* return code for success */
+#define EXIT 10      /* menu item number for EXIT */
+#define PI 3.14159265358979323846264338327950288
+
+
+#endif /* CONSTANTS_H */
+
